671ee726ce44a255862cd8cb9b06a4ea718285ed
Deep Bose <deepbose2012@gmail.com>
2021-12-08 20:42:23 +0600

    Initial Commit

---
+++ Restart/RestartApp.swift
@@ -0,0 +1,17 @@
+//
+//  RestartApp.swift
+//  Restart
+//
+//  Created by Deep Bose on 11/22/21.
+//
+
+import SwiftUI
+
+@main
+struct RestartApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
